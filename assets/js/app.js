65fa0437f3ab044c3ee26bed2410405d0ab05f8d
milan <pperisic.milan@gmail.com>
2022-02-25 23:02:03 +0100

    Added owl links

---
+++ assets/js/app.js
@@ -0,0 +1,22 @@
+// Owl carousel
+$('.owl-carousel').owlCarousel({
+  loop:true,
+  margin:10,
+  lazyLoad:true,
+  loop:true,
+  autoplay:true,
+  autoplayTimeout:3000,
+  autoplayHoverPause:true,
+  nav:true,
+  responsive:{
+      0:{
+          items:1
+      },
+      600:{
+          items:3
+      },
+      1000:{
+          items:3
+      }
+  }
+})
